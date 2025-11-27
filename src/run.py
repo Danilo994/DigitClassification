caaafccf97fd8d30855a4b7476a180c0718ae469
Danilo994 <daniloarrabal1@hotmail.com>
2025-11-27 12:05:32 -0300

    network2

--- src/run.py
+++ src/run.py
@@ -1,8 +1,14 @@
 import mnist_loader
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-import network
-net = network.Network([784, 30, 10])
+#import network
+#net = network.Network([784, 30, 10])
 #net = network.Network([784, 100, 10])
+#net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
-net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
+import network2
+#net = network2.Network([784, 30, 10])
+net = network2.Network([784, 100, 10], cost=network2.CrossEntropyCost)
+net.large_weight_initializer()
+#net.SGD(training_data, 30, 10, 0.5, evaluation_data=test_data, monitor_evaluation_accuracy=True)
+net.SGD(training_data, 30, 10, 0.5, evaluation_data=validation_data, monitor_evaluation_accuracy=True, monitor_training_cost=True)

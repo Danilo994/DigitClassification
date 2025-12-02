6d075fe4df01f7f6d84e613a945bf2048343bf93
Danilo994 <daniloarrabal1@hotmail.com>
2025-12-02 12:35:28 -0300

    flat error

--- src/network3.py
+++ src/network3.py
@@ -18,6 +18,7 @@ def load_data_shared(filename="../data/mnist.pkl.gz"):
     return [shared(tr), shared(va), shared(te)]
 
 class Network(object):
+
     def __init__(self, layers, mini_batch_size):
         self.layers = layers
         self.mini_batch_size = mini_batch_size
@@ -26,17 +27,16 @@ class Network(object):
             self.params += layer.params
 
     def forward(self, x, training=False):
+        if x.dim() == 2:
+            x= x.view(x.size(0), 1 , 28, 28)
         for layer in self.layers:
             x = layer.forward(x, training)
         return x
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta,
-            validation_data, test_data, lmbda=0.0):
-
+    def SGD(self, training_data, epochs, mini_batch_size, eta, validation_data, test_data, lmbda=0.0):
         train_x, train_y = training_data
         val_x, val_y = validation_data
         test_x, test_y = test_data
-
         train_loader = DataLoader(
             TensorDataset(train_x, train_y),
             batch_size=mini_batch_size, shuffle=True
@@ -49,7 +49,6 @@ class Network(object):
             TensorDataset(test_x, test_y),
             batch_size=mini_batch_size
         )
-
         optimizer = torch.optim.SGD(self.params, lr=eta, weight_decay=lmbda)
         loss_fn = nn.CrossEntropyLoss()
 
@@ -63,14 +62,15 @@ class Network(object):
                 loss.backward()
                 optimizer.step()
 
-                if batch_idx % 200 == 0:
+                if batch_idx % 1000 == 0:
                     print(f"Epoch {epoch}, batch {batch_idx}, loss={loss.item():.4f}")
 
+            # validação
             val_acc = self.accuracy_loader(val_loader)
             print(f"Epoch {epoch}: validation accuracy: {val_acc*100:.2f}%")
 
             if val_acc > best_val:
-                print("This is the best validation accuracy to date.")
+                print("Best result until now.")
                 best_val = val_acc
                 test_acc = self.accuracy_loader(test_loader)
                 print(f"Corresponding test accuracy: {test_acc*100:.2f}%")
@@ -86,22 +86,16 @@ class Network(object):
         return correct / total
 
 class ConvPoolLayer(object):
-    def __init__(self, filter_shape, image_shape, poolsize=(2,2),
-                 activation_fn=torch.sigmoid):
-
+    def __init__(self, filter_shape, image_shape, poolsize=(2,2), activation_fn=torch.sigmoid):
         out_channels, in_channels, kernel_h, kernel_w = filter_shape
-        self.conv = nn.Conv2d(in_channels, out_channels,
-                              kernel_size=(kernel_h, kernel_w))
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=(kernel_h, kernel_w))
         self.pool = nn.MaxPool2d(poolsize)
         self.activation_fn = activation_fn
-
         self.params = list(self.conv.parameters())
 
     def forward(self, x, training=False):
-        x = x.view(x.size(0), *self.conv.weight.shape[1:])
-        x = self.conv(x)
+        x = F.relu(self.conv(x))
         x = self.pool(x)
-        x = self.activation_fn(x)
         return x
 
 class FullyConnectedLayer(object):
@@ -110,23 +104,19 @@ class FullyConnectedLayer(object):
         self.activation_fn = activation_fn
         self.p_dropout = p_dropout
         self.dropout = nn.Dropout(p_dropout)
-
         self.params = list(self.linear.parameters())
 
     def forward(self, x, training=False):
         x = x.view(x.size(0), -1)
-
         if training and self.p_dropout > 0:
             x = self.dropout(x)
-
-        x = self.linear(x)
-        return self.activation_fn(x)
+        x = F.relu(self.linear(x))
+        return x
 
 class SoftmaxLayer(object):
     def __init__(self, n_in, n_out, p_dropout=0.0):
         self.linear = nn.Linear(n_in, n_out)
         self.dropout = nn.Dropout(p_dropout)
-
         self.params = list(self.linear.parameters())
 
     def forward(self, x, training=False):
